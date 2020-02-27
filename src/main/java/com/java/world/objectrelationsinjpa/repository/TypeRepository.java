bb9da797b6453de5e16848f8438450f3fbb4ad86
Errolito Lopez <errolito.lopez@whitecloak.com>
2020-02-27 10:57:53 +0800

    Remove

--- src/main/java/com/java/world/objectrelationsinjpa/repository/TypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.java.world.objectrelationsinjpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TypeRepository extends JpaRepository<TypeEntity, Long> {
-}
